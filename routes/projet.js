9ced4fef18633301232cb8b59d980afb3aead1be
yacine977 <yacine.tamindjoute97@gmail.com>
2024-05-07 02:47:26 +0200

    Ajout de routes pour obtenir un projet par ID, mettre à jour et supprimer un projet

--- routes/projet.js
+++ routes/projet.js
@@ -25,4 +25,45 @@ router.post("/", async function (req, res, next) {
   res.status(201).json(rows);
 });
 
+//route get projet by id
+router.get("/:id", async function (req, res, next) {
+  const [rows] = await pool.query("select * from projet where id = ?", [
+    req.params.id,
+  ]);
+  res.json(rows);
+});
+
+//route pour update projet
+router.put("/:id", async function (req, res, next) {
+  const {
+    nom,
+    description,
+    dateDebut,
+    dateFinPrevu,
+    dateFinReel,
+    chefDeProjetId,
+  } = req.body;
+  const [rows] = await pool.query(
+    "UPDATE Projet SET nom = ?, description = ?, dateDebut = ?, dateFinPrevu = ?, dateFinReel = ?, chefDeProjetId = ? WHERE id = ?",
+    [
+      nom,
+      description,
+      dateDebut,
+      dateFinPrevu,
+      dateFinReel,
+      chefDeProjetId,
+      req.params.id,
+    ]
+  );
+  res.json(rows);
+});
+
+//route pour delete projet
+router.delete("/:id", async function (req, res, next) {
+  const [rows] = await pool.query("DELETE FROM Projet WHERE id = ?", [
+    req.params.id,
+  ]);
+  res.json(rows);
+});
+
 module.exports = router;
