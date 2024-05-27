5c9316dec2ee379dd0a16d748b651f251bf79ac6
yacine977 <yacine.tamindjoute97@gmail.com>
2024-05-27 19:18:45 +0200

    Ajout de la route POST pour créer une nouvelle réunion

--- routes/reunion.js
+++ routes/reunion.js
@@ -8,4 +8,23 @@ router.get("/", async function (req, res, next) {
   res.json(rows);
 });
 
+//route pour créer une nouvelle réunion pour un projet donné
+router.post("/", async function (req, res) {
+  const { sujet, dateTime, projetId, createurId } = req.body;
+
+  try {
+    const [results] = await pool.query(
+      "INSERT INTO reunion (sujet, dateTime, projetId, createurId) VALUES (?, ?, ?, ?)",
+      [sujet, dateTime, projetId, createurId]
+    );
+
+    res
+      .status(201)
+      .json({ id: results.insertId, sujet, dateTime, projetId, createurId });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Erreur lors de la création de la réunion");
+  }
+});
+
 module.exports = router;
