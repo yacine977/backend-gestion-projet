04800b2606f1ccabe8c3cd41a61ae5d07c9dc8a5
yacine977 <yacine.tamindjoute97@gmail.com>
2024-06-20 00:26:24 +0200

    Ajout de la route pour récupérer les notifications d'un utilisateur

--- routes/notification.js
+++ routes/notification.js
@@ -2,7 +2,7 @@ var express = require('express');
 const { pool } = require('../services/database');
 var router = express.Router();
 
-
+//creer une nouvelle notification
 router.post('/nouvelle-notification', async (req, res) => {
     const { message, dateHeure, utilisateurId } = req.body;
     if (!message || !dateHeure || !utilisateurId) {
@@ -18,6 +18,30 @@ router.post('/nouvelle-notification', async (req, res) => {
       res.status(500).send({ error: 'Erreur serveur.' });
     }
 });
+
+// Récupérer les notifications d'un utilisateur
+router.get('/notifications/:utilisateurId', async (req, res) => {
+    const utilisateurId = req.params.utilisateurId;
+
+    try {
+        const query = 'SELECT * FROM notification WHERE utilisateurId = ?';
+        const [notifications] = await pool.query(query, [utilisateurId]);
+        if (notifications.length > 0) {
+            res.status(200).send(notifications);
+        } else {
+            res.status(404).send({ error: 'Aucune notification trouvée pour cet utilisateur.' });
+        }
+    } catch (error) {
+        console.error('Erreur lors de la récupération des notifications :', error);
+        res.status(500).send({ error: 'Erreur serveur.' });
+    }
+});
+
+
+
+
+
+
   
   module.exports = router;
 
